55afe5cfeb422b2f20c39add33e0f56ef46a7367
IHateNumber3 <lohudanynah@gmail.com>
2026-08-11 00:04:18 +0300

    Update inputsystem.cpp

--- inputsystem/inputsystem.cpp
+++ inputsystem/inputsystem.cpp
@@ -715,6 +715,17 @@ void CInputSystem::PollInputState_Platform()
 
 				case CocoaEvent_KeyDown:
 				{
+					case CocoaEvent_KeyDown:
+{
+    ButtonCode_t virtualCode;
+    if ( MapCocoaVirtualKeyToButtonCode( pEvent->m_VirtualKeyCode, &virtualCode ) )
+    {
+        // Закрываем экранную клавиатуру при нажатии Enter
+        if ( virtualCode == KEY_ENTER && SDL_IsTextInputActive() )
+        {
+            SDL_StopTextInput();
+        }
+
 					ButtonCode_t virtualCode;
 					if ( MapCocoaVirtualKeyToButtonCode( pEvent->m_VirtualKeyCode, &virtualCode ) )
 					{
