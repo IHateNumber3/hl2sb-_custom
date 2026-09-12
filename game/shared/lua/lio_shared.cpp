d25816d1a9931e8800d0184c7f625720aa63ff5b
IHateNumber3 <lohudanynah@gmail.com>
2026-09-12 12:04:20 +0300

    Update lio_shared.cpp

--- game/shared/lua/lio_shared.cpp
+++ game/shared/lua/lio_shared.cpp
@@ -1,144 +1,82 @@
 //========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
-//
-// Purpose: I/O system for Lua - Input/Output connections for entities
-//
+// Purpose: I/O system for Lua
 //=============================================================================//
 
 #include "cbase.h"
 #include "luamanager.h"
 #include "luasrclib.h"
-#include "lbaseentity_shared.h"
-
 #include "tier0/memdbgon.h"
 
-// I/O Output creation
-static int lua_CreateIOOutput(lua_State *L) {
-	CBaseEntity *pEnt = luaL_checkentity(L, 1);
-	const char *szName = luaL_checkstring(L, 2);
-	
-	if (!pEnt) {
-		lua_pushnil(L);
-		return 1;
-	}
-	
+static int lua_io_create_output(lua_State *L) {
 	lua_newtable(L);
-	lua_pushstring(L, "entity");
-	lua_pushentity(L, pEnt);
-	lua_settable(L, -3);
-	
-	lua_pushstring(L, "name");
-	lua_pushstring(L, szName);
-	lua_settable(L, -3);
-	
 	lua_pushstring(L, "type");
 	lua_pushstring(L, "output");
 	lua_settable(L, -3);
-	
-	lua_pushstring(L, "connections");
-	lua_newtable(L);
+	lua_pushstring(L, "name");
+	lua_pushstring(L, luaL_checkstring(L, 2));
 	lua_settable(L, -3);
-	
+	lua_pushstring(L, "entity");
+	lua_pushvalue(L, 1);
+	lua_settable(L, -3);
 	return 1;
 }
 
-// I/O Input creation
-static int lua_CreateIOInput(lua_State *L) {
-	CBaseEntity *pEnt = luaL_checkentity(L, 1);
-	const char *szName = luaL_checkstring(L, 2);
-	
-	if (!pEnt) {
-		lua_pushnil(L);
-		return 1;
-	}
-	
+static int lua_io_create_input(lua_State *L) {
 	lua_newtable(L);
-	lua_pushstring(L, "entity");
-	lua_pushentity(L, pEnt);
-	lua_settable(L, -3);
-	
-	lua_pushstring(L, "name");
-	lua_pushstring(L, szName);
-	lua_settable(L, -3);
-	
 	lua_pushstring(L, "type");
 	lua_pushstring(L, "input");
 	lua_settable(L, -3);
-	
+	lua_pushstring(L, "name");
+	lua_pushstring(L, luaL_checkstring(L, 2));
+	lua_settable(L, -3);
+	lua_pushstring(L, "entity");
+	lua_pushvalue(L, 1);
+	lua_settable(L, -3);
 	return 1;
 }
 
-// Connect input to output
-static int lua_IOConnect(lua_State *L) {
-	if (!lua_istable(L, 1) || !lua_istable(L, 2)) {
-		lua_pushboolean(L, false);
+static int lua_io_connect(lua_State *L) {
+	if (!lua_istable(L, 1)) {
+		lua_pushboolean(L, 0);
 		return 1;
 	}
-	
 	lua_getfield(L, 1, "connections");
-	if (lua_istable(L, -1)) {
-		int len = lua_objlen(L, -1);
-		lua_pushvalue(L, 2);
-		lua_rawseti(L, -2, len + 1);
-		lua_pop(L, 1);
-		lua_pushboolean(L, true);
-	} else {
-		lua_pop(L, 1);
-		lua_pushboolean(L, false);
+	if (!lua_istable(L, -1)) {
+		lua_newtable(L);
+		lua_setfield(L, 1, "connections");
+		lua_getfield(L, 1, "connections");
 	}
-	
+	lua_pushvalue(L, 2);
+	lua_rawseti(L, -2, luaL_len(L, -2) + 1);
+	lua_pushboolean(L, 1);
 	return 1;
 }
 
-// Fire an I/O input
-static int lua_FireIOInput(lua_State *L) {
-	if (!lua_istable(L, 1)) {
-		return 0;
-	}
-	
-	lua_getfield(L, 1, "entity");
-	CBaseEntity *pEnt = lua_toentity(L, -1);
-	lua_pop(L, 1);
-	
-	if (!pEnt) return 0;
-	
-	lua_getfield(L, 1, "name");
-	const char *szInputName = lua_tostring(L, -1);
-	lua_pop(L, 1);
-	
+static int lua_io_fire(lua_State *L) {
 	lua_getglobal(L, "hook");
 	if (lua_istable(L, -1)) {
 		lua_getfield(L, -1, "Call");
 		if (lua_isfunction(L, -1)) {
-			lua_pushstring(L, "OnIOInputFired");
-			lua_pushentity(L, pEnt);
-			lua_pushstring(L, szInputName);
-			
-			if (lua_gettop(L) > 4) {
-				lua_pushvalue(L, 3);
-			} else {
-				lua_pushnil(L);
-			}
-			
-			lua_pcall(L, 4, 0, 0);
+			lua_pushstring(L, "OnIOFired");
+			lua_pushvalue(L, 1);
+			lua_pcall(L, 2, 0, 0);
 		} else {
 			lua_pop(L, 1);
 		}
 	}
 	lua_pop(L, 1);
-	
 	return 0;
 }
 
-// Table of I/O functions
-static const luaL_Reg io_funcs[] = {
-	{"CreateIOOutput", lua_CreateIOOutput},
-	{"CreateIOInput", lua_CreateIOInput},
-	{"IOConnect", lua_IOConnect},
-	{"FireIOInput", lua_FireIOInput},
+static const luaL_Reg io_lib[] = {
+	{"CreateOutput", lua_io_create_output},
+	{"CreateInput", lua_io_create_input},
+	{"Connect", lua_io_connect},
+	{"Fire", lua_io_fire},
 	{NULL, NULL}
 };
 
-LUALIB_API int luaopen_io (lua_State *L) {
-	luaL_register(L, LUA_IOLIBNAME, io_funcs);
+LUALIB_API int luaopen_io(lua_State *L) {
+	luaL_register(L, "io", io_lib);
 	return 1;
 }
