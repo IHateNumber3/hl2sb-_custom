7f7eadaf4d7ca1a82a97be1b6f7d7bf59ec02090
IHateNumber3 <lohudanynah@gmail.com>
2026-09-12 21:18:44 +0300

    Update lio_shared.cpp

--- game/shared/lua/lio_shared.cpp
+++ game/shared/lua/lio_shared.cpp
@@ -1,14 +1,7 @@
-//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
-//
-// Purpose: I/O system for Lua
-//
-//=============================================================================//
-
 #include "cbase.h"
 #include "luamanager.h"
 #include "luasrclib.h"
 #include "lbaseentity_shared.h"
-
 #include "tier0/memdbgon.h"
 
 static int luasrc_ents_CreateIOOutput(lua_State *L) {
@@ -62,6 +55,10 @@ static int luasrc_ents_CreateIOInput(lua_State *L) {
 	lua_pushstring(L, "input");
 	lua_settable(L, -3);
 	
+	lua_pushstring(L, "connections");
+	lua_newtable(L);
+	lua_settable(L, -3);
+	
 	return 1;
 }
 
@@ -71,18 +68,90 @@ static int luasrc_ents_IOConnect(lua_State *L) {
 		return 1;
 	}
 	
+	// Add output to input's connections
 	lua_getfield(L, 1, "connections");
-	if (lua_istable(L, -1)) {
-		int idx = lua_objlen(L, -1);
-		lua_pushvalue(L, 2);
-		lua_rawseti(L, -2, idx + 1);
-		lua_pop(L, 1);
-		lua_pushboolean(L, true);
-		return 1;
+	if (!lua_istable(L, -1)) {
+		lua_newtable(L);
+		lua_setfield(L, 1, "connections");
+		lua_getfield(L, 1, "connections");
 	}
 	
-	lua_pushboolean(L, false);
+	int idx = lua_objlen(L, -1);
+	lua_pushvalue(L, 2);
+	lua_rawseti(L, -2, idx + 1);
+	lua_pop(L, 1);
+	
+	lua_pushboolean(L, true);
 	return 1;
+}
+
+static int luasrc_ents_FireIOInput(lua_State *L);
+
+static int luasrc_ents_FireIOOutput(lua_State *L) {
+	if (!lua_istable(L, 1)) {
+		return 0;
+	}
+	
+	int paramCount = lua_gettop(L);
+	
+	lua_getfield(L, 1, "entity");
+	if (lua_isnil(L, -1)) {
+		lua_pop(L, 1);
+		return 0;
+	}
+	CBaseEntity *pEnt = lua_toentity(L, -1);
+	lua_pop(L, 1);
+	
+	if (!pEnt) return 0;
+	
+	lua_getfield(L, 1, "name");
+	const char *szOutputName = lua_tostring(L, -1);
+	lua_pop(L, 1);
+	
+	lua_getglobal(L, "hook");
+	if (lua_istable(L, -1)) {
+		lua_getfield(L, -1, "Call");
+		if (lua_isfunction(L, -1)) {
+			lua_pushstring(L, "OnIOOutputFired");
+			lua_pushnil(L);
+			lua_pushentity(L, pEnt);
+			lua_pushstring(L, szOutputName ? szOutputName : "");
+			
+			if (paramCount > 1) {
+				lua_pushvalue(L, 2);
+			} else {
+				lua_pushnil(L);
+			}
+			
+			lua_pcall(L, 5, 0, 0);
+		} else {
+			lua_pop(L, 1);
+		}
+	}
+	lua_pop(L, 1);
+	
+	// Fire connected inputs
+	lua_getfield(L, 1, "connections");
+	if (lua_istable(L, -1)) {
+		int len = lua_objlen(L, -1);
+		for (int i = 1; i <= len; i++) {
+			lua_rawgeti(L, -1, i);
+			if (lua_istable(L, -1)) {
+				lua_pushcfunction(L, luasrc_ents_FireIOInput);
+				lua_pushvalue(L, -2);
+				
+				if (paramCount > 1) {
+					lua_pushvalue(L, 2);
+				}
+				
+				lua_pcall(L, paramCount > 1 ? 2 : 1, 0, 0);
+			}
+			lua_pop(L, 1);
+		}
+	}
+	lua_pop(L, 1);
+	
+	return 0;
 }
 
 static int luasrc_ents_FireIOInput(lua_State *L) {
@@ -90,7 +159,6 @@ static int luasrc_ents_FireIOInput(lua_State *L) {
 		return 0;
 	}
 	
-	// Сохраняем информацию о втором параметре ДО манипуляции
 	int paramCount = lua_gettop(L);
 	
 	lua_getfield(L, 1, "entity");
@@ -116,7 +184,6 @@ static int luasrc_ents_FireIOInput(lua_State *L) {
 			lua_pushentity(L, pEnt);
 			lua_pushstring(L, szInputName ? szInputName : "");
 			
-			// Если было 2+ параметра - передаём значение
 			if (paramCount > 1) {
 				lua_pushvalue(L, 2);
 			} else {
@@ -130,15 +197,36 @@ static int luasrc_ents_FireIOInput(lua_State *L) {
 	}
 	lua_pop(L, 1);
 	
+	// Fire connected outputs
+	lua_getfield(L, 1, "connections");
+	if (lua_istable(L, -1)) {
+		int len = lua_objlen(L, -1);
+		for (int i = 1; i <= len; i++) {
+			lua_rawgeti(L, -1, i);
+			if (lua_istable(L, -1)) {
+				lua_pushcfunction(L, luasrc_ents_FireIOOutput);
+				lua_pushvalue(L, -2);
+				
+				if (paramCount > 1) {
+					lua_pushvalue(L, 2);
+				}
+				
+				lua_pcall(L, paramCount > 1 ? 2 : 1, 0, 0);
+			}
+			lua_pop(L, 1);
+		}
+	}
+	lua_pop(L, 1);
+	
 	return 0;
 }
-
 
 static const luaL_Reg inout_funcs[] = {
 	{"CreateIOOutput", luasrc_ents_CreateIOOutput},
 	{"CreateIOInput", luasrc_ents_CreateIOInput},
 	{"IOConnect", luasrc_ents_IOConnect},
 	{"FireIOInput", luasrc_ents_FireIOInput},
+	{"FireIOOutput", luasrc_ents_FireIOOutput},
 	{NULL, NULL}
 };
 
